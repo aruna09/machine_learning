5643c24d473943f1c22c6abb89b9f833237ab5ab
ArunaMaurya221B <aruna.maurya12@gmail.com>
2018-04-12 20:11:30 +0530

    Parameters changed

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -29,7 +29,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 clf.fit(features_train, labels_train)
-print(clf.predict(features_train))
+print(clf.predict(features_test))
 #print predi
 
 
