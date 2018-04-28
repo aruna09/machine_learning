4fe9e5d0d8f9d753d34075d4433b9e5e3baef89d
ArunaMaurya221B <aruna.maurya12@gmail.com>
2018-04-28 18:56:35 +0530

    Stemmed out words using Snowball Stemmer

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -15,11 +15,12 @@ def parseOutText(f):
         text = parseOutText(f)
         
         """
+    stemmer = SnowballStemmer("english")
+
 
 
     f.seek(0)  ### go back to beginning of file (annoying)
     all_text = f.read()
-
     ### split off metadata
     content = all_text.split("X-FileName:")
     words = ""
@@ -28,17 +29,16 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
-
+        ###words = text_string
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+        sw = text_string.split(' ')
+        ###print sw
+        for i in sw:
+        	print stemmer.stem(i)
 
-
-
-
-    return words
+    return text_string
 
     
 
