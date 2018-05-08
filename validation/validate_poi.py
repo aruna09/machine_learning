a307f267d529570fae52d84dc10eb175494daa62
ArunaMaurya221B <aruna.maurya12@gmail.com>
2018-05-08 18:35:13 +0530

    Added code to split data into training and testing sets and then created a decision tree classifier to test the enron data given

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -25,8 +25,12 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import train_test_split
 
+features_train, features_test, labels_train, labels_test = train_test_split(features,labels, test_size=0.3, random_state=42)
 
-### it's all yours from here forward!  
-
-
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
